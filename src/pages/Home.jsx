8eb1ae35e533ec13f33d6fcc9b03080c35f25c04
david kim <dkreliable@gmail.com>
2025-06-14 10:19:18 -0400

    test do not touch added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,8 @@ const Home = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  // test do not touch !!!!
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
